766f7503ddeeb72964440abe28e280cf273326ab
guofeng <guofeng@dnjexpress.com>
2019-08-30 11:31:47 +0800

    基于iview admin web 初始化

---
+++ upms-web/src/config/index.js
@@ -0,0 +1,41 @@
+export default {
+  /**
+   * @description 配置显示在浏览器标签的title
+   */
+  title: 'upms-web',
+  /**
+   * @description 应用身份令牌用于请求接口时服务验证
+   */
+  appToken:'732E2D2A-7934-4679-B0FB-45E565D3303D',
+  /**
+   * @description token在Cookie中存储的天数，默认1天
+   */
+  cookieExpires: 7,
+  /**
+   * @description 是否使用国际化，默认为false
+   *              如果不使用，则需要在路由中给需要在菜单中展示的路由设置meta: {title: 'xxx'}
+   *              用来在菜单中显示文字
+   */
+  useI18n: true,
+  /**
+   * @description api请求基础路径
+   */
+  baseUrl: {
+    // dev: 'https://www.easy-mock.com/mock/5add9213ce4d0e69998a6f51/iview-admin/',
+	  dev: 'http://127.0.0.1:10000',
+    pro: 'https://produce.com'
+  },
+  /**
+   * @description 默认打开的首页的路由name值，默认为home
+   */
+  homeName: 'home',
+  /**
+   * @description 需要加载的插件
+   */
+  plugin: {
+    'error-store': {
+      showInHeader: true, // 设为false后不会在顶部显示错误日志徽标
+      developmentOff: true // 设为true后在开发环境不会收集错误信息，方便开发中排查错误
+    }
+  }
+}
---
+++ upms-web/src/store/module/user.js
@@ -0,0 +1,241 @@
+import {
+  login,
+  logout,
+  getUserPermission,
+  getMessage,
+  getContentByMsgId,
+  hasRead,
+  removeReaded,
+  restoreTrash,
+  getUnreadCount
+} from '@/api/user'
+import { setUser, getUser,getToken } from '@/libs/util'
+
+export default {
+  state: {
+    userName: getUser().userName,
+    userId: getUser().userId,
+    nickName: getUser().nickName,
+    avatarImgPath: '',
+    token: getToken(),
+    email: '',
+    phone: '',
+    orgId: getUser().orgId,
+    jobTitleId: getUser().jobTitleId,
+    access: '',
+    hasGetInfo: false,
+    unreadCount: 0,
+    messageUnreadList: [],
+    messageReadedList: [],
+    messageTrashList: [],
+    messageContentStore: {}
+  },
+  mutations: {
+    setUser (state, { ...user} ) {
+      state.userId = user.userId
+      state.userName = user.userName
+      state.nikeName = user.nikeName
+      state.token = user.token
+      state.email = user.email
+      state.phone = user.phone
+      state.orgId = user.orgId
+      state.jobTitleId = user.jobTitleId
+      setUser(user)
+    },
+    setAccess (state, access) {
+      state.access = access
+    },
+    setAvatar (state, avatarPath) {
+      state.avatarImgPath = avatarPath
+    },
+    setHasGetInfo (state, status) {
+      state.hasGetInfo = status
+    },
+    setMessageCount (state, count) {
+      state.unreadCount = count
+    },
+    setMessageUnreadList (state, list) {
+      state.messageUnreadList = list
+    },
+    setMessageReadedList (state, list) {
+      state.messageReadedList = list
+    },
+    setMessageTrashList (state, list) {
+      state.messageTrashList = list
+    },
+    updateMessageContentStore (state, { msg_id, content }) {
+      state.messageContentStore[msg_id] = content
+    },
+    moveMsg (state, { from, to, msg_id }) {
+      const index = state[from].findIndex(_ => _.msg_id === msg_id)
+      const msgItem = state[from].splice(index, 1)[0]
+      msgItem.loading = false
+      state[to].unshift(msgItem)
+    }
+  },
+  getters: {
+    messageUnreadCount: state => state.messageUnreadList.length,
+    messageReadedCount: state => state.messageReadedList.length,
+    messageTrashCount: state => state.messageTrashList.length
+  },
+  actions: {
+    // 登录
+    handleLogin ({ commit }, { userName, password }) {
+      userName = userName.trim()
+	    password = btoa(password)
+      return new Promise((resolve, reject) => {
+        login({
+          userName,
+          password
+        }).then(res => {
+          const data = res.data
+          if (data.result === 0) {
+            const user = data.data
+            commit('setUser',
+              {
+                userId: user.id,
+                userName: user.username,
+                nikeName: user.nickname,
+                token : user.token,
+                email : user.email,
+                phone : user.phone,
+                orgId : user.orgId,
+                jobTitleId : user.jobtitleId
+              })
+            resolve()
+          } else {
+			       reject(data.message)
+          }
+
+        }).catch(err => {
+          reject(err)
+        })
+      })
+    },
+    // 退出登录
+    handleLogOut ({ state, commit }) {
+      return new Promise((resolve, reject) => {
+        logout(state.token).then(() => {
+          commit('setToken', '')
+          commit('setAccess', [])
+          resolve()
+        }).catch(err => {
+          reject(err)
+        })
+        // 如果你的退出登录无需请求接口，则可以直接使用下面三行代码而无需使用logout调用接口
+        // commit('setToken', '')
+        // commit('setAccess', [])
+        // resolve()
+      })
+    },
+    // 获取用户权限相关信息
+    getUserPermission ({ state, commit }) {
+      return new Promise((resolve, reject) => {
+        try {
+          getUserPermission(state.userId, state.token).then(res => {
+            const data = res.data
+            if (data.result===0) {
+              commit('setAccess', data.data.permissionKey)
+              commit('setHasGetInfo', true)
+              resolve(data)
+            }else {
+              reject(data.message)
+            }
+          }).catch(err => {
+            reject(err)
+          })
+        } catch (error) {
+          reject(error)
+        }
+      })
+    },
+    // 此方法用来获取未读消息条数，接口只返回数值，不返回消息列表
+    getUnreadMessageCount ({ state, commit }) {
+      getUnreadCount().then(res => {
+        const { data } = res
+        commit('setMessageCount', data)
+      })
+    },
+    // 获取消息列表，其中包含未读、已读、回收站三个列表
+    getMessageList ({ state, commit }) {
+      return new Promise((resolve, reject) => {
+        getMessage().then(res => {
+          const { unread, readed, trash } = res.data
+          commit('setMessageUnreadList', unread.sort((a, b) => new Date(b.create_time) - new Date(a.create_time)))
+          commit('setMessageReadedList', readed.map(_ => {
+            _.loading = false
+            return _
+          }).sort((a, b) => new Date(b.create_time) - new Date(a.create_time)))
+          commit('setMessageTrashList', trash.map(_ => {
+            _.loading = false
+            return _
+          }).sort((a, b) => new Date(b.create_time) - new Date(a.create_time)))
+          resolve()
+        }).catch(error => {
+          reject(error)
+        })
+      })
+    },
+    // 根据当前点击的消息的id获取内容
+    getContentByMsgId ({ state, commit }, { msg_id }) {
+      return new Promise((resolve, reject) => {
+        let contentItem = state.messageContentStore[msg_id]
+        if (contentItem) {
+          resolve(contentItem)
+        } else {
+          getContentByMsgId(msg_id).then(res => {
+            const content = res.data
+            commit('updateMessageContentStore', { msg_id, content })
+            resolve(content)
+          })
+        }
+      })
+    },
+    // 把一个未读消息标记为已读
+    hasRead ({ state, commit }, { msg_id }) {
+      return new Promise((resolve, reject) => {
+        hasRead(msg_id).then(() => {
+          commit('moveMsg', {
+            from: 'messageUnreadList',
+            to: 'messageReadedList',
+            msg_id
+          })
+          commit('setMessageCount', state.unreadCount - 1)
+          resolve()
+        }).catch(error => {
+          reject(error)
+        })
+      })
+    },
+    // 删除一个已读消息到回收站
+    removeReaded ({ commit }, { msg_id }) {
+      return new Promise((resolve, reject) => {
+        removeReaded(msg_id).then(() => {
+          commit('moveMsg', {
+            from: 'messageReadedList',
+            to: 'messageTrashList',
+            msg_id
+          })
+          resolve()
+        }).catch(error => {
+          reject(error)
+        })
+      })
+    },
+    // 还原一个已删除消息到已读消息
+    restoreTrash ({ commit }, { msg_id }) {
+      return new Promise((resolve, reject) => {
+        restoreTrash(msg_id).then(() => {
+          commit('moveMsg', {
+            from: 'messageTrashList',
+            to: 'messageReadedList',
+            msg_id
+          })
+          resolve()
+        }).catch(error => {
+          reject(error)
+        })
+      })
+    }
+  }
+}
